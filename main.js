d9304dc54efc1f53e92698151f5ce67f702f9fa3
JuN-front <junsei1511280530@gmail.com>
2025-02-19 20:54:33 +0900

    ボタン修正

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-const ham = document.querySelector("#js-hamburger");
-const nav = document.querySelector("#js-nav");
-
-ham.addEventListener("click", function () {
-  ham.classList.toggle("active");
-  nav.classList.toggle("active");
-});
+document.getElementById("navbtn").onclick = function () {
+  document.querySelector("html").classList.toggle("open");
+};
