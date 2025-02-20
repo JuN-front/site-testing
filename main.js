794f7a52a1ab2544318c9b72b6123ca5d90b5cc4
JuN-front <junsei1511280530@gmail.com>
2025-02-20 09:21:59 +0900

    cssの修正です

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-$(function () {
-  // ハンバーガーメニューをクリックするとactiveクラスが付与される
-  $(".hamburger").click(function () {
-    $(this).toggleClass("active");
+window.onload = function () {
+  var nav = document.getElementById("nav-wrapper");
+  var hamburger = document.getElementById("js-hamburger");
+  var blackBg = document.getElementById("js-black-bg");
+
+  hamburger.addEventListener("click", function () {
+    nav.classList.toggle("open");
   });
-});
+  blackBg.addEventListener("click", function () {
+    nav.classList.remove("open");
+  });
+};
