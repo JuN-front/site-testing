a09216f7b1ac09cd516c555cbf2fd7188d911353
JuN-front <junsei1511280530@gmail.com>
2025-03-27 01:54:36 +0900

    event-list update test

--- main.js
+++ main.js
@@ -10,3 +10,36 @@ window.onload = function () {
     nav.classList.remove("open");
   });
 };
+
+const button1 = document.getElementById("1st-page");
+
+const button2 = document.getElementById("2nd-page");
+
+const button3 = document.getElementById("3rd-page");
+
+const screen1 = document.querySelector(".event-section-1");
+
+const screen2 = document.querySelector(".event-section-2");
+
+const screen3 = document.querySelector(".event-section-3");
+
+button1.addEventListener("click", function () {
+  button2.classList.remove("active");
+  button3.classList.remove("active");
+  screen2.classList.remove("active");
+  screen3.classList.remove("active");
+});
+
+button2.addEventListener("click", function () {
+  button1.classList.remove("active");
+  button3.classList.remove("active");
+  screen1.classList.remove("active");
+  screen3.classList.remove("active");
+});
+
+button3.addEventListener("click", function () {
+  button1.classList.remove("active");
+  button2.classList.remove("active");
+  screen1.classList.remove("active");
+  screen2.classList.remove("active");
+});
