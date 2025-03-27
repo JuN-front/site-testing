5839d633b94befe4f5cf0c059485f337e748e727
JuN-front <junsei1511280530@gmail.com>
2025-03-27 15:41:38 +0900

    fixing trial

--- main.js
+++ main.js
@@ -9,43 +9,27 @@ window.onload = function () {
   blackBg.addEventListener("click", function () {
     nav.classList.remove("open");
   });
-
-  const button1 = document.getElementById("button-1");
-
-  const button2 = document.getElementById("button-2");
-
-  const button3 = document.getElementById("button-3");
-
-  const screen1 = document.querySelector(".event-section-1");
-
-  const screen2 = document.querySelector(".event-section-2");
-
-  const screen3 = document.querySelector(".event-section-3");
-
-  button1.addEventListener("click", function () {
-    screen1.classList.remove("active");
-    screen2.classList.remove("active");
-    screen3.classList.remove("active");
-    button1.classList.remove("active");
-    button2.classList.remove("active");
-    button3.classList.remove("active");
-  });
-
-  button2.addEventListener("click", function () {
-    screen1.classList.add("active");
-    screen2.classList.add("active");
-    screen3.classList.remove("active");
-    button1.classList.add("active");
-    button2.classList.add("active");
-    button3.classList.remove("active");
-  });
-
-  button3.addEventListener("click", function () {
-    screen1.classList.add("active");
-    screen2.classList.remove("active");
-    screen3.classList.add("active");
-    button1.classList.add("active");
-    button2.classList.remove("active");
-    button3.classList.add("active");
-  });
 };
+
+const tabItems = document.querySelectorAll(".button");
+
+tabItems.forEach((button) => {
+  button.addEventListener("click", () => {
+    // すべてのタブを非アクティブにする
+    tabItems.forEach((t) => {
+      t.classList.remove("active");
+    });
+    // すべてのコンテンツを非表示にする
+    const Events = document.querySelectorAll(".event-section");
+    Events.forEach((event) => {
+      event.classList.remove("active");
+    });
+
+    // クリックされたタブをアクティブにする
+    button.classList.add("active");
+
+    // 対応するコンテンツを表示
+    const tabIndex = Array.from(tabItems).indexOf(button);
+    Events[tabIndex].classList.add("active");
+  });
+});
